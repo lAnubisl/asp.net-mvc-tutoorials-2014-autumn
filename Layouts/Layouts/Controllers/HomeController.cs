feb127615d7a98dbe8155f57cd9814426b9beff2
lAnubisl <alexander.panfilenok@gmail.com>
2014-03-15 17:12:20 +0300

    Fix some issues in Razor project

---
+++ Layouts/Layouts/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layouts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectToRouteResult Index()
+        {
+            return RedirectToAction("Cats");
+        }
+
+        public ActionResult Cats()
+        {
+            return View();
+        }
+
+        public ActionResult Dogs()
+        {
+            return View();
+        }
+	}
+}
